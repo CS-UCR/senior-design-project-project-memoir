f1904ad42e581fa7ed7f8b7649f9b721ebe0eada
Bryan Orozco <bryanorozcoweb@gmail.com>
2022-01-12 23:00:27 -0800

    dev: Updated apollo client
    Working apollo client with api key implementation

--- iOS-frontend/ProjectM/Delegates/AppDelegate.swift
+++ iOS-frontend/ProjectM/Delegates/AppDelegate.swift
@@ -8,23 +8,6 @@ import ARKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     var settings: NSDictionary!
-    
-    // this should be converted to a shared helper function class in the future
-    // converts custom key/values from Info.plt into a settings dictionary
-    // ref: http://www.shannongrizzell.me/2019/12/07/using-plist-part-1-reading-plist-files/
-    
-    func getPropertyList(name: String) -> NSDictionary {
-
-        // First let's get the path to our property list
-        guard let path = Bundle.main.path(forResource: name, ofType: "plist"),
-            let xml = FileManager.default.contents(atPath: path) else {
-        // If the property list doesn't exist, we'll throw an error for now.
-            fatalError("Unable to load the requested property list")
-        }
-        // With data read into the xml variable we can now decode into a dictionary object.
-        let propertyDictionary = try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as? NSDictionary
-        return propertyDictionary!
-    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
         // Make sure user device supports ARWorldTrackingConfiguration
@@ -34,21 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // If user's device does not support, terminate app
             fatalError("Your device does not support ARKit.")
         }
-        
-        
-        // fetch and confirm query is functioning
-        Network.shared.apollo.fetch(query: TestQueryQuery()) { result in
-          switch result {
-          case .success(let graphQLResult):
-            print("Success! Result: \(graphQLResult)")
-          case .failure(let error):
-            print("Failure! Error: \(error)")
-          }
-        }
-        
-        // load settings dictionary from Info.plist file
-        // note: you can add custom key/values by adding hardcoded variables into AppSettings.xcconfig and then adding that pair inside of Info.plist (look at the key "GraphQL URL" for an example)
-        settings = getPropertyList(name: "Info");
         
         // return true if user's device supports ARWorldTracking
         return true
--- iOS-frontend/ProjectM/Networking/Network.swift
+++ iOS-frontend/ProjectM/Networking/Network.swift
@@ -9,29 +9,30 @@ import Apollo
 
 class Network {
     
-    var settings: NSDictionary!
-    static let shared = Network() // singleton object (no duplicate connections)
+//    guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
+//        throw Error.missingKey
+//    }
+    static let shared = Network()
     
-    // this should be converted to a shared helper function class in the future
-    // converts custom key/values from Info.plt into a settings dictionary
-    // ref: http://www.shannongrizzell.me/2019/12/07/using-plist-part-1-reading-plist-files/
-    func getPropertyList(name: String) -> NSDictionary {
+    private(set) lazy var apollo: ApolloClient = {
 
-        // First let's get the path to our property list
-        guard let path = Bundle.main.path(forResource: name, ofType: "plist"),
-            let xml = FileManager.default.contents(atPath: path) else {
-        // If the property list doesn't exist, we'll throw an error for now.
-            fatalError("Unable to load the requested property list")
-        }
-        // With data read into the xml variable we can now decode into a dictionary object.
-        let propertyDictionary = try? PropertyListSerialization.propertyList(from: xml, options: .mutableContainersAndLeaves, format: nil) as? NSDictionary
-        return propertyDictionary!
-    }
-    
-    init() {
-        settings = getPropertyList(name: "Info")
-    }
-    
-    private(set) lazy var apollo = ApolloClient(
-        url: try! URL(jsonValue: settings["GraphQL URL"]!))
+        let url: String = try! Configuration.value(for: "GRAPHQL_URL")
+        let api_key: String = try! Configuration.value(for: "API_KEY")
+        
+        let headers = [
+            "x-api-key": api_key
+        ]
+
+        let client = URLSessionClient()
+        let cache = InMemoryNormalizedCache()
+        let store = ApolloStore(cache: cache)
+        let interceptorProvider = DefaultInterceptorProvider(client: client, store: store)
+        let transport = RequestChainNetworkTransport(
+            interceptorProvider: interceptorProvider,
+            endpointURL: URL(string: "https://" + url)!,
+            additionalHeaders: headers
+        )
+        return ApolloClient(networkTransport: transport, store: store)
+        
+    }()
 }
