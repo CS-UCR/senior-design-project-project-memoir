03ae950bb20fe400bca22c9afb38d921d43df5ad
Marco Alexi Sta Ana <msta@Marcos-Air.lan1>
2022-01-31 00:48:56 -0800

    added storyboards and login capability

--- iOS-frontend/ProjectM/AppleIDButtonAuthorizaiton.swift
+++ iOS-frontend/ProjectM/AppleIDButtonAuthorizaiton.swift
@@ -1,43 +1,59 @@
+////
+////  AppleIDButtonAuthorizaiton.swift
+////  ProjectM
+////
+////  Created by Carlos Loeza on 10/26/21.
+////
+//import AuthenticationServices
+//import UIKit
 //
-//  AppleIDButtonAuthorizaiton.swift
-//  ProjectM
+//@IBDesignable
+//class AppleIDButtonAuthorizaiton: UIButton {
 //
-//  Created by Carlos Loeza on 10/26/21.
+//    public var authorizationButton = ASAuthorizationAppleIDButton()
+//    
+//    
+//    
+//    
+//    // init's allow our button to used in storyboard
+//    override public init(frame: CGRect) {
+//        super.init(frame: frame)
+//    }
 //
-import AuthenticationServices
-import UIKit
-
-@IBDesignable
-class AppleIDButtonAuthorizaiton: UIButton {
-
-    private var authorizationButton: ASAuthorizationAppleIDButton!
-    
-    // init's allow our button to used in storyboard
-    override public init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    override public func draw(_ rect: CGRect) {
-        super.draw(rect)
-
-        // Create ASAuthorizationAppleIDButton
-        authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
-
-        // Show authorizationButton
-        addSubview(authorizationButton)
-
-        // Use auto layout to make authorizationButton follow the MyAuthorizationAppleIDButton's dimension
-        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            authorizationButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 0.0),
-            authorizationButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0.0),
-            authorizationButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0.0),
-            authorizationButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0.0),
-        ])
-    }
-
-}
+//    required public init?(coder aDecoder: NSCoder) {
+//        super.init(coder: aDecoder)
+//    }
+//
+//    override public func draw(_ rect: CGRect) {
+//        super.draw(rect)
+//
+//        // Create ASAuthorizationAppleIDButton
+//        authorizationButton = ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
+//
+//        // Show authorizationButton
+//        addSubview(authorizationButton)
+//
+//        // Use auto layout to make authorizationButton follow the MyAuthorizationAppleIDButton's dimension
+//        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+//        NSLayoutConstraint.activate([
+//            authorizationButton.topAnchor.constraint(equalTo: self.topAnchor, constant: 0.0),
+//            authorizationButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0.0),
+//            authorizationButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0.0),
+//            authorizationButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0.0),
+//        ])
+//    }
+//
+//
+//    // Adding functionality to Apple Login
+//
+//    // Check if user clicked on sign in if did....
+//    @objc func didTapSignIn(){
+//        let provider = ASAuthorizationAppleIDProvider()
+//        let request = provider.createRequest()
+//        request.requestedScopes = [.fullName, .email]
+//    }
+//
+//
+//
+//
+//}
--- iOS-frontend/ProjectM/ViewController.swift
+++ iOS-frontend/ProjectM/ViewController.swift
@@ -5,17 +5,69 @@
 //  Created by Carlos Loeza on 10/8/21.
 //
 
-
+import AuthenticationServices
 import UIKit
 
 class ViewController: UIViewController {
 
-   
+    private let authorizationButton = ASAuthorizationAppleIDButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(authorizationButton)
+        authorizationButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
         // Do any additional setup after loading the view.
     }
+    
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        authorizationButton.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
+        authorizationButton.center = view.center
+    }
 
+     @objc func didTapSignIn(){
+         // Grab provider object from AppleID kit
+         let provider = ASAuthorizationAppleIDProvider()
+         
+         // Requests information from user's Apple Account
+         let request = provider.createRequest()
+         request.requestedScopes = [.fullName, .email]
+         
+         let controller = ASAuthorizationController(authorizationRequests: [request])
+         
+         // Perform Requests
+         controller.delegate = self
+         controller.presentationContextProvider = self
+         controller.performRequests()
+     }
 
 }
 
+// Handles Authorization delegation if error or success
+extension ViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("error")
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential{
+        case let credentials as ASAuthorizationAppleIDCredential:
+            let firstName = credentials.fullName?.givenName
+            let lastName = credentials.fullName?.familyName
+            let email = credentials.email
+            
+            break
+        default:
+            break
+        }
+    }
+}
+
+
+// Handles
+extension ViewController: ASAuthorizationControllerPresentationContextProviding{
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+}
