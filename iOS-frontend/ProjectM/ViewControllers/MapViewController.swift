f79596f8c984c7a362389e397d6fd520550f6bc2
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-03-18 15:05:45 -0700

    App now has map showing user location and zoomed in view of apple maps

--- iOS-frontend/ProjectM/ViewControllers/MapViewController.swift
+++ iOS-frontend/ProjectM/ViewControllers/MapViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 class MapViewController: UIViewController {
     
     private let locationManager = CLLocationManager()
+    private var currentCoordinates: CLLocationCoordinate2D?
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -24,9 +25,29 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    
     private func configureLocationServices() {
         locationManager.delegate = self
+        // location status
+        let status = CLLocationManager.authorizationStatus()
+        // check if we have user's location
+        if status == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdates(locationManager: locationManager)
+        }
+    }
+    
+    private func zoomToCurrentLocation(with coordinate: CLLocationCoordinate2D) {
+        let zoomRegion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 500,longitudinalMeters: 500)
+        mapView.setRegion(zoomRegion, animated: true)
+    }
+    
+    
+    private func beginLocationUpdates(locationManager: CLLocationManager){
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        // get longtitude and latitude of current location
+        locationManager.startUpdatingLocation()
     }
     
 }
@@ -34,10 +55,18 @@ class MapViewController: UIViewController {
 
 extension MapViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let latestLocation = locations.first else {return}
         
+        if currentCoordinates == nil {
+            zoomToCurrentLocation(with: latestLocation.coordinate)
+        }
+        
+        currentCoordinates = latestLocation.coordinate
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-    
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdates(locationManager: manager)
+        }
     }
 }
