65b0543b68409fd6a0279c8e24ebacc11a4ac454
CarlosLoeza <cloeza1@mail.ccsf.edu>
2022-03-18 13:24:59 -0700

    Create a mapview so user's can see messages posted on a map

---
+++ iOS-frontend/ProjectM/ViewControllers/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  ProjectM
+//
+//  Created by Carlos Loeza on 3/18/22.
+//
+
+import ARKit
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController {
+    
+}
